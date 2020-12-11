ad7d1150a9be46738467daaf9b77781f1268953a
Dario Cangialosi <dario.cangialosi@gmail.com>
2020-12-11 19:15:20 +0100

    var source, words // made these local variables now, they were global vars

--- newparse.js
+++ newparse.js
@@ -5,54 +5,56 @@ var variables={}
 function out(...args){ console.log(...args) }
 function out1(arg){ console.log(arg) }
 
-var source, words
-
 function test_sequence_basic(){
-//source='if3|expr|3|==|expr|1|+|2|"Equal"|"Not-Equal"'
-source='out1|10|out1|20'
-words=source.split("|")
+//var source='if3|expr|3|==|expr|1|+|2|"Equal"|"Not-Equal"'
+var source='out1|10|out1|20'
+var words=source.split("|")
 /*
-var nodes=nodes_build(0)
+var nodes=nodes_build(words,0)
 out("result:\t",(nodes[0])) // JSON.stringify(nodes[0]) // debugging
 out("node_exec:",node_exec(nodes[0]))
 */
-node_exec(nodes_build(0)[0])
-node_exec(nodes_build(2)[0])
-var sequence=[nodes_build(0)[0],nodes_build(2)[0]]
+node_exec(nodes_build(words,0)[0])
+node_exec(nodes_build(words,2)[0])
+var sequence=[nodes_build(words,0)[0],nodes_build(words,2)[0]]
 out(sequence)
 node_exec(sequence)
 }
 
 function test_sequence_basic2(){
     out("test_sequence_basic2()")
-source='set|"n"|1|out1|expr|get|"n"|+|10'
-words=source.split("|")
-node_exec(nodes_build(0)[0])
+var source='set|"n"|1|out1|expr|get|"n"|+|10'
+var words=source.split("|")
+node_exec(nodes_build(words,0)[0])
 out("variables", variables)
-node_exec(nodes_build(3)[0])
+node_exec(nodes_build(words,3)[0])
 }
 
 function test_sequence_basic3(){
         out("test_sequence_basic3()")
-    source='begin|set|"n"|2|out1|get|"n"|out1|expr|get|"n"|+|10'
-    words=source.split("|")
-    var nodes=nodes_build(0)[0]
+    var source='begin|set|"n"|2|out1|get|"n"|out1|expr|get|"n"|+|10'
+    var words=source.split("|")
+    var nodes=nodes_build(words,0)[0]
     out(JSON.stringify(nodes)) // for debugging
     node_exec(nodes)
-    //node_exec(nodes_build(3)[0])
+    //node_exec(nodes_build(words,3)[0])
 }
 
 test_loop()
 function test_loop(){
     out("test_loop()")
-    source='begin|set|"n"|1|loop|expr|get|"n"|"<"|11|begin|out1|if3|expr|0|"=="|expr|get|"n"|"%"|2|expr|get|"n"|"+"|" numero pari"|get|"n"|set|"n"|expr|get|"n"|+|1|end'
-    exec_source()
+    var source='begin|set|"n"|1|loop|expr|get|"n"|"<"|11|begin|out1|if3|expr|0|"=="|expr|get|"n"|"%"|2|expr|get|"n"|"+"|" numero pari"|get|"n"|set|"n"|expr|get|"n"|+|1|end'
+    exec_source(source)
 }
 
-function exec_source(){
-    words=source.split("|")
-    var nodes=nodes_build(0)[0]
-    out(JSON.stringify(nodes)) // for debugging
+function exec_source(source){
+    var words=source.split("|")
+    var nodes=nodes_build(words,0)[0]
+    var debug=false
+    if(debug){
+        out(nodes)
+        out(JSON.stringify(nodes))
+    }
     node_exec(nodes)
 }
 
@@ -89,14 +91,14 @@ function loop(condition,while_true_do_this){
     }
 }
 
-function nodes_build(word_index){
+function nodes_build(words,word_index){
     var current_word=words[word_index]
     if(current_word=="begin"){ // sequence
         var size=0
         var sequence=[]
         var cur_index=word_index+1
         while(cur_index<words.length && words[cur_index]!="end"){
-            var [out_nodes,total_size]=nodes_build(cur_index)
+            var [out_nodes,total_size]=nodes_build(words,cur_index)
             sequence.push(out_nodes)
             cur_index+=total_size
             size+=total_size
@@ -110,7 +112,7 @@ function nodes_build(word_index){
     word_index++
     var total_size=1
     for(var count=1;count<=current_arity; count++){
-        var [built_node,size]=nodes_build(word_index)
+        var [built_node,size]=nodes_build(words,word_index)
         out_nodes.push(built_node)
         word_index+=size
         total_size+=size
